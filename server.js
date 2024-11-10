ee0210e09430bc377e2b55e80563d13014e596ec
BelisarioEduriel <belisarioeduriel@gmail.com>
2024-11-10 15:41:40 -0400

    actualizacion de P2_29781854

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get __filename and __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index", {
